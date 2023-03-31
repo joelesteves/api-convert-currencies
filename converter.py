986a024ccf67a6a1157a5fb5f71aab2a5991e38a
Joel Esteves <joelesteves@Joels-Air.bbrouter>
2023-03-31 09:07:42 -0300

    refactor converter & add schamas params body

--- converter.py
+++ converter.py
@@ -1,24 +1,44 @@
-from os import getenv
+import os
 import requests
 import aiohttp
 from fastapi import HTTPException
 
-ALPHAVANTAGE_APIKEY = getenv('ALPHAVANTAGE_APIKEY')
+API_KEY = os.getenv('API_KEY')
 
-async def sync_converter(from_currency: str, to_currencies: str, price: float):
-    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currencies}&apikey={ALPHAVANTAGE_APIKEY}'
+def sync_converter(from_currency: str, to_currency: str, price: float):
     
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
+    
+    try:
+        response = requests.get(url=url)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=f'Internal error on alpha vantange requests: {error}')
+    
+    data = response.json()
+    
+    if "Realtime Currency Exchange Rate" not in data:
+        raise HTTPException(status_code=400, detail='Probably invalid currencies given')
+    
+    return float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+
+async def async_converter(from_currency: str, to_currency: str, price: float):
+    
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey=UB83QBMYTDJYS3NF'
+    print(url)
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url=url) as response:
                 data = await response.json()
-            
+
     except Exception as error:
-        raise HTTPException(status_code=400, detail=error)
+        raise HTTPException(status_code=500, detail=f'Internal error on alpha vantange requests: {error}')
     
     if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=400, detail='Realtime Currency Exchange Rate not in data')
-    
-    exchange_rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate']) 
-    
-    return price * exchange_rate
+        raise HTTPException(status_code=400, detail='Probably invalid currencies given') 
+
+    result = {
+        to_currency: float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]) * price
+    }
+
+    return result
