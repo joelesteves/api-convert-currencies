0127617a179f8f5ce6f655b25663090e7ee26290
Joel Esteves <joelesteves@Joels-Air.bbrouter>
2023-03-31 09:08:05 -0300

    refactor converter & add schamas params body routes

--- routes.py
+++ routes.py
@@ -1,16 +1,21 @@
 from asyncio import gather
-from fastapi import APIRouter
-from converter import sync_converter
+from fastapi import APIRouter, Path, Query
+from converter import async_converter
+from schamas import ConverterInput, ConverterOutput
 router = APIRouter()
 
 @router.get('/convert/{from_currency}')
-async def convert(from_currency: str, to_currencies: str, price: float):
+async def convert(
+    from_currency: str = Path(max_length=50, regex='^[A-Z]{3}$'),
+    to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price: float = Query(gt=0)
+):
     to_currencies = to_currencies.split(',')
     
     corotines = []
     
     for currency in to_currencies:
-        response = sync_converter(
+        response = async_converter(
             from_currency=from_currency,
             to_currencies=currency,
             price=price
@@ -19,3 +24,28 @@ async def convert(from_currency: str, to_currencies: str, price: float):
                 
     result = await gather(*corotines)
     return result
+
+@router.get('/async/v2/{from_currency}',response_model=ConverterOutput)
+async def converter(
+    body: ConverterInput,
+    from_currency: str = Path(max_length=50, regex='^[A-Z]{3}$'),
+):    
+    corotines = []
+    
+    to_currencies = body.to_currencies
+    price = body.price
+
+    for currency in to_currencies:
+        response = async_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+        corotines.append(response)
+
+    result = await gather(*corotines)
+    
+    return ConverterOutput(
+        response_message='Successfully converted',
+        converted_prices=result
+    )
